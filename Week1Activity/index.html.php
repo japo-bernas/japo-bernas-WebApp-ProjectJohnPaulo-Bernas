c54bb3cd0aaefadd990822813f178587c5f400d5
japo-bernas <johnpaulobernas@gmail.com>
2024-09-20 15:42:40 +0800

    Add files via upload

---
+++ Week1Activity/index.html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="Stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>SWOOSH WHOOP👌<h1>
+    <p>Welcome to the World of basketball<p>
+    <p style=color:white;">
+   
+
+
+    <?php
+    Echo "Howdy fella!!! ";
+    echo "Enjoy reading articles about your favorite basketball players.";
+
+    ?>
+    
+</body>
+</html>
